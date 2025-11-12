5b3b578743cb20cf2ce6a8f8a59c1470cd32348d
Judith Sitero <jsitero@my.yearupunited.org>
2025-11-11 18:55:49 -0800

    update

--- src/main/java/com/pluralsight/ComplexModels/SignaturePizza.java
+++ src/main/java/com/pluralsight/ComplexModels/SignaturePizza.java
@@ -7,10 +7,24 @@ import java.util.List;
 public class SignaturePizza extends Pizza {
     public SignaturePizza(String name, String size, String crust, String sauce, boolean stuffedCrust, List<Topping> defaultToppings) {
         super(size, crust, sauce, stuffedCrust);
-        super.name = name;
+//        this.name = name;
+//        super.name = name;
+        this.setName(name);
         for (Topping t : defaultToppings) {
             super.addTopping(t);
         }
+    }
+    // Regular Toppings (Regular/Sauces cost 0 for inclusion)
+    public static Topping createRegular(String name) {
+        return new Topping(name, false, 0.00);
+    }
+    // Premium Meat Toppings
+    public static Topping createMeat(String name) {
+        return new Topping(name, true, 1.50);
+    }
+    // Premium Cheese Toppings
+    public static Topping createCheese(String name) {
+        return new Topping(name,true, 0.90);
     }
 
 
